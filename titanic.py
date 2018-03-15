ded69b656129dbbc7e47ac5f996f7d3e35ea0d8d
leon <18850544602@163.com>
2018-03-15 20:01:37 +0800

    # 第三次提交 - 正确率 : 0.53110 ## 修改 - 把分类器改成回归模型, 模型正确率反而下降了..

--- titanic.py
+++ titanic.py
@@ -4,6 +4,9 @@ import os
 import lightgbm as lgb
 
 
+# TODO 增加对缺失值的处理
+# TODO 写一个交叉验证框架
+# TODO 尝试对 lightGBM 学习参数的调整
 def main():
     # 对数据进行处理, 分用于训练模型
     train = load_data("train.csv")
@@ -22,7 +25,7 @@ def load_data(filename, sep=","):
 
 
 def select_feature(train_data):
-    selected_columns = ["Age", "Sex", "Pclass","Fare"]
+    selected_columns = ["Age", "Sex", "Pclass", "Fare"]
     train = train_data[selected_columns]
     return train
 
@@ -31,8 +34,8 @@ def map_feature(train_data):
     # 将性别映射为 [0,1]
     train_data.Sex = train_data.Sex.map(lambda sex: 0 if 'male' == sex else 1)
     # 将上船费用归一化
-    train_data.Fare = (train_data.Fare - train_data.Fare.min()) / \
-                      (train_data.Fare.max() - train_data.Fare.min())
+    train_data.Fare = (train_data.Fare - train_data.Fare.min()) / (
+                train_data.Fare.max() - train_data.Fare.min())
     return train_data
 
 
@@ -42,7 +45,7 @@ def handle_na(train_data):
 
 def train_model(train_data, labels):
     print("train model...")
-    classifier = lgb.LGBMClassifier()
+    classifier = lgb.LGBMRegressor()
     return classifier.fit(train_data, labels)
 
 
@@ -51,6 +54,7 @@ def get_result_and_save(classfier):
     test = load_data("test.csv", ",")
     test = map_feature(test)
     res = classfier.predict(select_feature(test))
+    res = list(map(lambda x: 1 if x > 0.5 else 0, res))
     print("save...")
     submission = pd.DataFrame({
         'PassengerId': test['PassengerId'], 'Survived': res
