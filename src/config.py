5bc7d63e2acb22ac29c2b45dc619a937410dde8d
leon <18850544602@163.com>
2018-03-23 21:13:15 +0800

    ## 修改 - 调整项目结构 - 修改文件的相关参数

--- src/config.py
+++ src/config.py
@@ -1,37 +1,44 @@
+"""
+    文件配置
+"""
 # 数据文件配置
 test_file_name = "data/train_data/test.csv"
 train_file_name = "data/train_data/train.csv"
-# 日志文件配置
-log_file_name = "src/log/model_performance.log"
+# 模型 performance 文件配置
+performance_file_name = "src/log/model_performance.txt"
+performance_file_mode = "a+"
 # 结果文件配置
 submission_file_name = "data/submission/submission.csv"
 # 参数存储文件配置
-params_file_name = "src/params/model.txt"
+params_file_mode = "w"
+tree_params_file_name = "src/params/model.txt"
+params_file_name = "src/params/params.txt"
+
 # 分隔符
 csv_seperator = ","
-# GridSearch 中的参数调整
+"""
+    GridSearch 中的参数调整
+"""
 n_threads = 2  # CPU cores
 metric = ['binary_error', 'auc']
 
-# cv_learning_rates = [0.01, 0.05, 0.001, 0.005]
-# cv_boosting_type = ['dart', 'gbdt', 'goss']
-# cv_feature_fraction = [0.7, 0.8, 0.9, 1]
-# cv_n_estimators = [i * 50 for i in range(1, 10)]
-# 通过学习固定参数
+# 通过学习固定的参数
 cv_learning_rates = [0.05]
 cv_boosting_type = ['gbdt']
 cv_n_estimators = [100]
 cv_feature_fraction = [0.9]
 
 # 用于防止模型过拟合的参数
-# cv_lambda_l1 = [0.9, 1.2, 1, 1.5]
-# cv_lambda_l2 = [0.9, 1.2, 1, 1.5]
-# 通过学习固定参数
+# 通过学习后的固定参数
 cv_num_leaves = [10]
 cv_lambda_l1 = [0.9]
 cv_lambda_l2 = [1]
-# 最后得到的模型参数
-cv_split_size = 0.10
+cv_bagging_fraction = [0.9]
+cv_bagging_freq = [0]
+"""
+    最后得到的模型参数
+"""
+cv_split_size = 0.07
 boosting_type = 'gbdt'
 n_estimators = 100
 feature_fraction = 0.9
@@ -41,6 +48,14 @@ verbose_eval = 50
 num_leaves = 10
 lambda_l1 = 0.9
 lambda_l2 = 1
-# 控制画图
-is_plot_tree = False
-is_show_metric = True
+bagging_fraction = 0.9
+bagging_freq = 0
+
+"""
+    项目中某些功能的控制开关
+"""
+
+can_plot_tree = False
+can_show_metric = True
+can_grid_search_hyper_params = True
+can_save_best_params = True
--- src/titanic.py
+++ src/titanic.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import logging as log
+
 import sys
 
 import lightgbm as lgb
@@ -14,29 +14,20 @@ from src import config
 
 
 # TODO 引入 stacking
-# TODO 引入交叉验证选择最佳模型
-# TODO 合并 map_feature 和 select_feature 两个函数
+
 
 def main():
-    config_log()
+    # config_log()
     train_data, labeled_size, labels, Ids = pre_process_data()
-    bestParams = get_offline_result(train_data, labeled_size, labels)
-    model = train_model(bestParams, train_data, labeled_size, labels)
+    if config.can_grid_search_hyper_params:
+        search_object = search_hyper_parameters(train_data, labeled_size,
+                                                labels)
+        if config.can_save_best_params:
+            save_best_params(search_object)
+
+    model = train_model(train_data, labeled_size, labels)
     evaluate_and_save(model, train_data, labeled_size, Ids)
     sys.exit(0)
-
-
-def config_log(log_filename=config.log_file_name):
-    """
-        配置输出格式
-    :param log_filename: log 日志文件的存放地址
-    :return:
-    """
-    log.basicConfig(level=log.INFO,
-                    format='%(asctime)s %(filename)s[line:%(lineno)d] %('
-                           'levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S', filename=log_filename,
-                    filemode='a+')
 
 
 def pre_process_data(train_filename=config.train_file_name,
@@ -61,65 +52,13 @@ def load_data(filename, sep=config.csv_seperator):
     return pd.read_csv(filename, sep=sep)
 
 
-def get_offline_result(train_data, labeled_size, labels):
-    x_train, x_test, y_train, y_test = train_test_split(
-        train_data[:labeled_size], labels[:labeled_size],
-        test_size=config.cv_split_size, random_state=42)
-    gridParams = {
-        'learning_rate': config.cv_learning_rates,
-        'n_estimators': config.cv_n_estimators,
-        'num_leaves': config.cv_num_leaves,
-        'boosting_type': config.cv_boosting_type, 'objective': ['binary'],
-        'lambda_l1': config.cv_lambda_l1, 'lambda_l2': config.cv_lambda_l2,
-        'random_state': [501], 'feature_fraction': config.cv_feature_fraction,
-        # 'colsample_bytree': [0.64, 0.65, 0.66], 'subsample': [0.7, 0.75],
-        # 'reg_alpha': [1, 1.2], 'reg_lambda': [1, 1.2, 1.4],
-        }
-    clf = lgb.LGBMRegressor(boosting_type='dart', objective='binary')
-    grid = GridSearchCV(clf, gridParams, verbose=1, cv=5, n_jobs=-1)
-    grid.fit(x_train, y_train)
-    return grid
-
-
-def train_model(grid, train, labeled_size, labels):
-    params = {
-        'boosting_type': config.boosting_type, 'objective': 'binary',
-        'metric': config.metric, 'learning_rate': config.learning_rate,
-        # 根据 GridSearchCV 中的参数进行设置
-        'nthread': config.n_threads, 'n_estimators': config.n_estimators,
-        'feature_fraction': 0.9, 'random_state': 501,
-        'lambda_l1': config.lambda_l1, 'lambda_l2': config.lambda_l2,
-        'max_depth': -1, 'num_leaves': config.num_leaves,
-        }
-    x_train, x_test, y_train, y_test = train_test_split(train[:labeled_size],
-                                                        labels[:labeled_size],
-                                                        test_size=config.cv_split_size,
-                                                        random_state=42)
-    train_data = lgb.Dataset(data=x_train, label=y_train)
-    test_data = lgb.Dataset(data=x_test, label=y_test)
-    eval_results = {}
-    log.info('\ngrid best params {}'.format(grid.best_params_))
-    gbm = lgb.train(params, train_set=train_data, num_boost_round=2000,
-                    valid_sets=[train_data, test_data],
-                    evals_result=eval_results, verbose_eval=50,
-                    early_stopping_rounds=config.early_stopping_rounds)
-    log.info("best iterations {}".format(gbm.best_iteration))
-    predicted = np.round(gbm.predict(x_test))
-    record_performance(y_test, predicted)
-    show_model_performance(gbm, eval_results)
-    return gbm
-
-
-def evaluate_and_save(clf, train, labeled_size, Ids):
-    # 存储模型参数
-    clf.save_model(config.params_file_name)
-    test = train[labeled_size:]
-    res = list(map(lambda x: 1 if x == 1.0 else 0, np.round(
-        clf.predict(test, num_iteration=clf.best_iteration))))
-
-    pd.DataFrame({
-        'PassengerId': Ids[labeled_size:], 'Survived': res
-        }).to_csv(config.submission_file_name, index=False)
+def handle_na(train_data):
+    # 年龄 和 费用用平均值填充
+    train_data.Age = train_data.Age.fillna(train_data.Age.median())
+    train_data.Fare = train_data.Fare.fillna(train_data.Fare.median())
+    # 上船港口默认为 S
+    train_data.Embarked = train_data.Embarked.fillna('S')
+    return train_data
 
 
 def extract_features(train_data):
@@ -165,23 +104,87 @@ def map_feature(train_data):
     return data_dum
 
 
-def handle_na(train_data):
-    # 年龄 和 费用用平均值填充
-    train_data.Age = train_data.Age.fillna(train_data.Age.median())
-    train_data.Fare = train_data.Fare.fillna(train_data.Fare.median())
-    # 上船港口默认为 S
-    train_data.Embarked = train_data.Embarked.fillna('S')
-    return train_data
+def search_hyper_parameters(train_data, labeled_size, labels):
+    x_train, x_test, y_train, y_test = train_test_split(
+        train_data[:labeled_size], labels[:labeled_size],
+        test_size=config.cv_split_size, random_state=42)
+    gridParams = {
+        'learning_rate': config.cv_learning_rates,
+        'n_estimators': config.cv_n_estimators,
+        'num_leaves': config.cv_num_leaves,
+        'boosting_type': config.cv_boosting_type, 'objective': ['binary'],
+        'lambda_l1': config.cv_lambda_l1, 'lambda_l2': config.cv_lambda_l2,
+        'random_state': [501], 'feature_fraction': config.cv_feature_fraction,
+        'bagging_fraction': config.cv_bagging_fraction,
+        'bagging_freq': config.cv_bagging_freq,
+        }
+    clf = lgb.LGBMRegressor(boosting_type='dart', objective='binary')
+    grid = GridSearchCV(clf, gridParams, verbose=1, cv=5, n_jobs=-1)
+    grid.fit(x_train, y_train)
+    return grid
+
+
+def save_best_params(search_object, filename=config.params_file_name,
+                     mode=config.params_file_mode):
+    with open(filename, mode) as writer:
+        writer.write('best params after grid search\n')
+        for param_key in search_object.best_params_:
+            writer.write("{} : {}\n".format(param_key,
+                                            search_object.best_params_[
+                                                param_key]))
+        writer.write("------------------------------\n")
+
+
+def train_model(train, labeled_size, labels):
+    params = {
+        'boosting_type': config.boosting_type, 'objective': 'binary',
+        'metric': config.metric, 'learning_rate': config.learning_rate,
+        # 根据 GridSearchCV 中的参数进行设置
+        'nthread': config.n_threads, 'n_estimators': config.n_estimators,
+        'feature_fraction': 0.9, 'random_state': 501,
+        'lambda_l1': config.lambda_l1, 'lambda_l2': config.lambda_l2,
+        'max_depth': -1, 'num_leaves': config.num_leaves,
+        'bagging_fraction': config.bagging_fraction,
+        'bagging_freq': config.bagging_freq,
+        }
+    x_train, x_test, y_train, y_test = train_test_split(train[:labeled_size],
+                                                        labels[:labeled_size],
+                                                        test_size=config.cv_split_size,
+                                                        random_state=42)
+    train_data = lgb.Dataset(data=x_train, label=y_train)
+    test_data = lgb.Dataset(data=x_test, label=y_test)
+    eval_results = {}
+
+    gbm = lgb.train(params, train_set=train_data, num_boost_round=2000,
+                    valid_sets=[train_data, test_data],
+                    evals_result=eval_results, verbose_eval=50,
+                    early_stopping_rounds=config.early_stopping_rounds)
+    predicted = np.round(gbm.predict(x_test))
+    record_performance(y_test, predicted)
+    show_model_performance(gbm, eval_results)
+    return gbm
+
+
+def evaluate_and_save(clf, train, labeled_size, Ids):
+    # 存储模型参数
+    clf.save_model(config.tree_params_file_name)
+    test = train[labeled_size:]
+    res = list(map(lambda x: 1 if x == 1.0 else 0, np.round(
+        clf.predict(test, num_iteration=clf.best_iteration))))
+
+    pd.DataFrame({
+        'PassengerId': Ids[labeled_size:], 'Survived': res
+        }).to_csv(config.submission_file_name, index=False)
 
 
 def show_model_performance(gbm, evals_result):
     # show model importance
     # lgb.plot_importance(gbm)
     # Show Decision Tree
-    if config.is_plot_tree:
-        graph = lgb.create_tree_digraph(gbm, name='Tree84')
+    if config.can_plot_tree:
+        graph = lgb.create_tree_digraph(gbm, name='Decision Tree')
         graph.render(view=True)
-    if config.is_show_metric:
+    if config.can_show_metric:
         fig, axs = plt.subplots(2, 1, figsize=(8, 10))
         for index in range(len(config.metric)):
             lgb.plot_metric(evals_result, config.metric[index],
@@ -189,12 +192,14 @@ def show_model_performance(gbm, evals_result):
     plt.show()
 
 
-def record_performance(y_test, predicted):
-    log.info('score is: {}\n'.format(accuracy_score(y_test, predicted)))
-    log.info(' \n {}'.format(classification_report(y_test, predicted,
-                                                   target_names=['Died',
-                                                                 'Survived'])))
-    log.info("------------------------------\n")
+def record_performance(y_test, predicted, filename=config.performance_file_name,
+                       mode=config.performance_file_mode):
+    with open(filename, mode) as writer:
+        writer.write('score is: {}\n'.format(accuracy_score(y_test, predicted)))
+        writer.write(' \n {}'.format(classification_report(y_test, predicted,
+                                                           target_names=['Died',
+                                                                         'Survived'])))
+        writer.write("------------------------------\n")
 
 
 if __name__ == "__main__":
